ae36586902bd6f9ca8ed2a7e4d4b33b9c8ca81bb
unknown <karolis.gric@gmail.com>
2016-05-19 18:43:31 +0300

    WTF?

--- peanuts/peanutpoll.js
+++ peanuts/peanutpoll.js
@@ -175,6 +175,7 @@ var Voter =  React.createClass({
                     var checker = null;
                     if (i===leng){
                       checker = prompt("Please enter your option", listas[0].name);
+                      console.log(checker);
                       console.log(prompt("Please enter your option", listas[0].name));
 
                     }
